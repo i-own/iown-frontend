096f3ce861460f5c16ad8c13dc35ef0336093c08
eagle <aparokshambhargav@gmail.com>
2024-12-09 20:18:33 +0530

    fix: build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import { Navbar } from "@/components/Navbar";
+// import { Navbar } from "@/components/Navbar";
 import { BackgroundEffects } from "@/components/BackgroundEffects";
 
 const Home: NextPage = () => {
